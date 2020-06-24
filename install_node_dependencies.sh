64920b2e4e55d4224771768b3136ef5b6b091cf7
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2020-06-24 18:02:09 +0200

    updated dependencies

---
+++ install_node_dependencies.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+cd Display/displayClient
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../displayService
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../../RoomCommandManager
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../Source/TACSourceService
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../TACSource
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../MockSourceService
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
+
+cd ../ActiveTraumaService
+
+/c/Windows/System32/cmd.exe //c 'start cmd /c npm install'
